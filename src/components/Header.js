a067e1697769779e3be2c5b04a00311ffad11270
Marta Rubio <93192647+Martarubio93@users.noreply.github.com>
2022-07-08 15:15:06 +0100

    name change

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import '../styles/layout/Header.scss'
 const Header = () => {
     return(
         <header className="header">
-            <h1 className="header__title">Callsign Challenge </h1>
+            <h1 className="header__title">Accordion Challenge </h1>
         </header>
     )
 }
